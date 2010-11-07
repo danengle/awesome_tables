7f25aa98d34fd1c3eaebf711551370cdf471278f
Dan Engle <engle.68@gmail.com>
2010-11-07 16:57:24 -0600

    added passing in blocks to t.column. Cleaned up display code

--- lib/awesome_tables.rb
+++ lib/awesome_tables.rb
@@ -1,12 +1,21 @@
 module AwesomeTables
   
-  class NoBlockGivenException < Exception
+  class Error < StandardError; end
+
+  class NoBlockGiven < Error; end
+  
+  class UnknownTableType < Error; end 
+  
+  class NoPartial < Error; end
+
+  class BadData < Error; end
+  
+  def self.register(table_type, &block)
+    AwesomeTables::AwesomeTable::register(table_type, &block)
   end
   
-  class UnknownTableTypeException < Exception 
-  end 
-  
-  class NoPartialException < Exception
+  def self.new(table_type, collection = [], options = {})
+    AwesomeTables::AwesomeTable::new(table_type, collection, options)
   end
 end
 
